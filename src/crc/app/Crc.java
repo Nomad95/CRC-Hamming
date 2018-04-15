667815a36eb7332fd598218c3ec807ea2cc2fda7
Igor Kopeć <igokop99@gmail.com>
2018-04-15 08:45:20 +0200

    initial

---
+++ src/crc/app/Crc.java
@@ -0,0 +1,30 @@
+package app;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Crc extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        
+        Scene scene = new Scene(root, 640, 480);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("CRC + Huffman");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
